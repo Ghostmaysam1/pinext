c7591f1b84c500f9d0321562ca2d37921c3a255d
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-09-19 21:48:51 +0330

    refactor: remove unused className

--- src/components/home/portfolio/components/projectBox/ProjectBox.jsx
+++ src/components/home/portfolio/components/projectBox/ProjectBox.jsx
@@ -15,7 +15,7 @@ function ProjectBox({description, image}) {
                 <img src={image} onLoad={handleImageLoad} className='h-full bg-center rounded-t-[12px]' alt={description} />
             </div>
             <div className="h-[55px] min-h-[55px] w-full border rounded-b-[12px] bg-primary-100 border-[#5E5E66] absolute bottom-0 left-0 flex justify-center items-center">
-                <p className='text-body'>{description}</p>
+                <p>{description}</p>
                 <button className="absolute font-iransans cursor-pointer right-6 flex justify-around items-center gap-1 w-fit border border-[#5E5E66] rounded-full px-5 py-1">صدا <span><img src={voiceIcon} alt="" /></span></button>
             </div>
         </div>
