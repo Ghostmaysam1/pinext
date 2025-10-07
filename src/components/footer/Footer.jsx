7b27e167b0802110283b5850cef9cca136d1215d
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-10-07 09:46:17 +0330

    feat: Update Footer text and add ServiceBox styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <div className="h-[250px] w-full flex flex-col items-center justify-between pb-10">
             <img src="logo-bottom.png" className="w-[156px]" alt="" />
             <span className="block w-[80%] h-1 rounded-full bg-[#D3D3D3]"></span>
-            <p className="text-[#8A8A8A]">صاحب امتیاز تمامی حقوق وب سایت، متعلق به پینکست  می‌باشد.</p>
+            <p className="text-[#8A8A8A]">صاحب امتیاز تمامی حقوق وب سایت، متعلق به پینکست می‌باشد.</p>
         </div>
     )
 }
--- src/components/home/Services/components/serviceBox/ServiceBox.jsx
+++ src/components/home/Services/components/serviceBox/ServiceBox.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { LayoutTemplate, Smartphone, Boxes, Headset } from "lucide-react";
+import './serviceBox.css';
 
 function ServiceBox({ icon, title, description, state }) {
     const [isOpen, setIsOpen] = useState(state ? true : false);
@@ -9,26 +10,27 @@ function ServiceBox({ icon, title, description, state }) {
     }
 
     return (
-        <div
-            onClick={handleClick}
-            className={`shadow-md transition-all duration-100 ease-in-out overflow-hidden w-[600px] border border-[#9E9EA735] p-5 rounded-[16px] flex flex-col gap-4 ${isOpen ? 'h-[300px]' : 'h-[100px] cursor-pointer'
-                }`}>
-            <div className="flex justify-end gap-2 transition duration-300 ease-in-out items-center" style={isOpen ? { flexDirection: 'column-reverse', alignItems: 'flex-end' } : {}}>
-                <h4>{title}</h4>
-                <span className="w-[60px] h-[60px] flex justify-center items-center rounded-full bg-secondary">
-                    {
-                        icon == 'smartphone' ?
-                        <Smartphone size={30} /> : icon == 'boxes' ?
-                        <Boxes size={30} /> : icon == 'layout-template' ?
-                        <LayoutTemplate size={30} /> : icon == 'headset' ?
-                        <Headset size={30} /> : ''
-                    }
-                </span>
-            </div>
-            <div className="flex justify-end text-right">
-                <p>{description}</p>
-            </div>
+        <div className='service-box relative'>
+            <div
+                onClick={handleClick}
+                className={`shadow-md transition-all duration-100 ease-in-out overflow-hidden w-[600px] border border-[#9E9EA735] p-5 rounded-[16px] flex flex-col gap-4 ${isOpen ? 'h-[300px]' : 'h-[100px] cursor-pointer'}`}>
+                <div className="flex justify-end gap-2 transition duration-300 ease-in-out items-center" style={isOpen ? { flexDirection: 'column-reverse', alignItems: 'flex-end' } : {}}>
+                    <h4>{title}</h4>
+                    <span className="w-[60px] h-[60px] flex justify-center items-center rounded-full bg-secondary">
+                        {
+                            icon == 'smartphone' ?
+                                <Smartphone size={30} /> : icon == 'boxes' ?
+                                    <Boxes size={30} /> : icon == 'layout-template' ?
+                                        <LayoutTemplate size={30} /> : icon == 'headset' ?
+                                            <Headset size={30} /> : ''
+                        }
+                    </span>
+                </div>
+                <div className="flex justify-end text-right">
+                    <p>{description}</p>
+                </div>
 
+            </div>
         </div>
     )
 }
